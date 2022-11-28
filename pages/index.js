f4a6af6fd5b78819304d0738522e191c7bd74388
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-11-27 23:12:51 -0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+
+import Grid from "@mui/material/Grid";
+import Sidebar from "../Component/Sidebar";
+import Main from "../Component/Home";
+
+import Footer from "../Component/Footer";
+export default function Home() {
+  return (
+    <Grid item xs={12} container>
+      <Grid item xs={12} display={"flex"} justifyContent={"space-between"} >
+        <Sidebar />
+        <Main/>
+      </Grid>
+      <Grid item sx={{ position: "relative" }}>
+        <Footer />
+      </Grid>
+    </Grid>
+  );
+}
